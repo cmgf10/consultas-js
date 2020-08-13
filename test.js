0c632c29a1d8a1347ee65ac9bfcfea0582d69c38
Carlos Guilherme <Cmgf10@gmail.com>
2020-08-12 21:54:25 -0300

    initial comit

---
+++ test.js
@@ -0,0 +1,8 @@
+const screenshot = require('./screenshot')
+const fs = require('fs')
+
+;(async () => {
+  const buffer = await screenshot('https://www.google.com')
+  fs.writeFileSync('screenshot.png', buffer.toString('binary'), 'binary')
+  console.log("cheguei")
+})()
